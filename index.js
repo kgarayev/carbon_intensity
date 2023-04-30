58ab50970e3de30c53247fbfe8b01860d5908714
kgarayev <kenangarayev@gmail.com>
2023-04-30 20:53:38 +0100

    setting up the files

---
+++ index.js
@@ -0,0 +1,19 @@
+// set a variable with the API URL
+const API_URL = `https://api.carbonintensity.org.uk/regional/postcode/{postcode}`;
+
+// variable for the data from API
+let apiData;
+
+// select the html element(s)
+const container = document.getElementById("container");
+
+// to simplify console.log command
+const { log } = console;
+
+// get data from the user
+
+// a function to get data from the API
+
+// display the obtained data for the user
+
+// update the data for the user
