349934034c4bf3ca3902e8e0fb03131d741287b5
kgarayev <kenangarayev@gmail.com>
2023-04-30 23:54:41 +0100

    first attempts at DOM manipulation

--- index.js
+++ index.js
@@ -51,13 +51,15 @@ const getData = async (postcode) => {
   const { data } = await axios.get(API_URL.replace("{postcode}", postcode));
 
   apiData = data.data[0];
-  log(apiData.data[0].generationmix);
+  log(apiData.shortname);
+  return apiData;
 };
 
 // check and run the data function
-const writeData = () => {
+const writeData = async () => {
   if (finalPostcode.length > 0) {
-    getData(finalPostcode);
+    const data = await getData(finalPostcode);
+    displayData(data);
     return;
   } else {
     return;
@@ -66,11 +68,20 @@ const writeData = () => {
 
 // display the obtained data for the user
 const displayData = (data) => {
-  const region = data.shortname;
-  const intensity = data.data[0].intensity;
-  const generationMix = data.data[0].generationmix;
+  if (data) {
+    const region = data.shortname;
+    const intensity = data.data[0].intensity;
+    const generationMix = data.data[0].generationmix;
 
-  const html = ``;
+    const html1 = `   <h4>Region: ${region}</h4>
+                          <h4>Carbon Intensity</h4>
+                          <p>Forecast: ${intensity.forecast}<p>
+                          <p>Index: ${intensity.index}<p>
+                          <h4>Generation Mix</h4>
+                          <p><p>`;
+
+    container.innerHTML = html1;
+  }
 };
 
 // update the data for the user
